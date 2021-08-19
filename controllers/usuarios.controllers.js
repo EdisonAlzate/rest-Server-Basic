1a765f2b71d50fb81461ccf2ea3c99d20d199c68
ealzate <ealzate@pratechgroup.com>
2021-08-18 21:01:31 -0500

    crud completo en node

--- controllers/usuarios.controllers.js
+++ controllers/usuarios.controllers.js
@@ -1,13 +1,30 @@
 const { response } = require("express")
-const { findOne } = require("../models/usuarios")
+const { findOne, findByIdAndUpdate } = require("../models/usuarios")
 const usuarios = require("../models/usuarios")
 const Usuarios = require("../models/usuarios")
 const bcrypt = require('bcrypt');
 
 
-const obtenerUsuario=(req, res=response)=> {
-       res.json('gets desde controllers')
+const obtenerUsuario=async(req, res=response)=> {
+
+    const {limit=5,desde=0}=req.query
+    const query={estado:true}
+
+    const [total,usuarios]= await  Promise.all([
+        Usuarios.countDocuments(query),
+        Usuarios.find(query)
+                .skip(Number(desde))
+                .limit(Number(limit))
+    ])
+   
+    
+    res.json({
+        total,
+        usuarios
+
+    })
    }
+
 const crearUsuario=async(req, res=response)=> {
    
     const {nombre,correo,password,rol}=req.body
@@ -50,8 +67,16 @@ const actualizarUsuario= async(req, res=response)=> {
     })
 }
 
-const eliminarUsuario=(req, res=response)=> {
-    res.send('eliminar desde controllers ')
+const eliminarUsuario=async(req, res=response)=> {
+    const {id}=req.params
+    
+   const usuarioEliminado= await Usuarios.findByIdAndUpdate(id,{estado:false})
+    
+    res.json({
+        msg:id,
+        usuarioEliminado
+    }
+    )
 }
 const patchUsuario=(req, res=response)=> {
     res.send('patch desde controllers ')
